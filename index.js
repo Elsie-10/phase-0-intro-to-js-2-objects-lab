e7d839517ef17805e45ca58b082a347591a956a0
Elsie <elsieoscar06@gmail.com>
2025-02-27 12:28:03 +0300

    using objects

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {};
+
+function updateEmployeeWithKeyAndValue (employee, key, value){
+    return {...employee, [key]: value };
+    // Return a new obect wih the original data and the new key-value pair
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // Directly modify the original employee object
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    // Create a new object by spreading the employee object and excluding the specified key
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Directly delete the key from the original employee object
+    delete employee[key];
+    return employee;
+  }
+  
